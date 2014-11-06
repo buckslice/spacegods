a1becde8366ffae94bda634dd32dbaa79696c331
David Galanter <dgalante@uci.edu>
2014-11-06 01:50:53 -0800

    Added planets that spawn in random locations off screen regardless of the changing camera.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (10, 0, 0) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+	
+	public GameObject muhThang;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (muhThang, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
