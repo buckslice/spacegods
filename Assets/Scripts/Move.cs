740535d5c0b3d5f65a8426e428ab5f2901fdaa43
David Galanter <dgalante@uci.edu>
2014-11-06 02:17:54 -0800

    Slightly fixed my bug

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 
+	float x, y;
 	// Use this for initialization
 	void Start () {
-	
+
+		if (transform.position.x < 0 && transform.position.y < 0) {
+						x = Random.Range(1.0f, 10.0f);
+						y = Random.Range (1.0f, 10.0f);
+				} else if (transform.position.x > 0 && transform.position.y < 0) {
+						x = Random.Range(-10.0f, -1.0f);
+						y = Random.Range (1.0f, 10.0f);
+				} else if (transform.position.x < 0 && transform.position.y > 0) {
+						x = Random.Range(1.0f, 10.0f);
+						y = Random.Range (-10.0f, -1.0f);
+				} else {
+						x = Random.Range(-10.0f, -1.0f);
+						y = Random.Range (-10.0f, -1.0f);
+				}
+
+		InvokeRepeating ("DeletePlanet", 10.0f, 5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (new Vector3 (10, 0, 0) * Time.deltaTime);
+		transform.Translate (new Vector3 (x, y, 0f) * Time.deltaTime);
+
+
 	}
+
+	public void DeletePlanet()
+	{
+		Destroy (gameObject);
+		--Planet.planetNum;
+		}
 }
