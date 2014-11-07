cc5dc766f40e4a092d92dc89c0d58048b809b0b3
jfc3 <buckslice@gmail.com>
2014-11-07 00:15:00 -0800

    Made planets collide with each other and Gods. Moved gravity to physics system. Renamed and reorganized files and stopped tracking Library folder.

---
+++ Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlanetGravity : MonoBehaviour {
+
+    public float gravity = 1f;  // should make sun class eventually and move this there
+
+    private Transform gravitationTarget;
+    private Rigidbody2D myRigidBody;
+
+    void Start() {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        gravitationTarget = GameObject.Find("Sun").transform;
+
+        // add some random velocity tangent to the direction of gravity
+        Vector3 dir = (gravitationTarget.transform.position - myRigidBody.transform.position).normalized;
+        Vector3 tangent = Vector3.Cross(dir, new Vector3(0, 0, Random.Range(-1f, 1f)));
+        
+        myRigidBody.velocity = 10f * tangent;
+
+    }
+
+    void Update() {
+        // add gravity force towards sun
+        Vector3 g = (gravitationTarget.transform.position - myRigidBody.transform.position).normalized * gravity;
+        myRigidBody.AddForce(g * myRigidBody.mass);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        // kill planet if it hits sun
+        if (collision.gameObject.tag == "Sun") {
+            Destroy(gameObject);
+            --PlanetSpawner.planetNum;
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpawner : MonoBehaviour {
+
+    public GameObject planet;
+    static public int planetNum = 0;
+
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("SpawnPlanet", 1f, 1f);
+    }
+
+    public void SpawnPlanet() {
+        if (planetNum < 50) {
+
+            float x = 0f;
+            float y = 0f;
+
+            // with Random.Range for integers the max is exclusive for some reason
+            switch (Random.Range(1, 5)) {
+                case 1:
+                    x = -0.1f;
+                    y = Random.Range(0f, 1f);
+                    break;
+
+                case 2:
+                    x = 1.1f;
+                    y = Random.Range(0, 1f);
+                    break;
+
+                case 3:
+                    y = -0.1f;
+                    x = Random.Range(0f, 1f);
+                    break;
+
+                case 4:
+                    y = 1.1f;
+                    x = Random.Range(0f, 1f);
+                    break;
+
+            }
+
+            Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(x, y, 10f));
+            GameObject newPlanet = (GameObject)Instantiate(planet, p, Quaternion.identity);
+            newPlanet.transform.parent = gameObject.transform;
+            ++planetNum;
+        }
+    }
+}
