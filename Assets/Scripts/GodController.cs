7482c3f5d20010c88e871a290bbe4eff04b0929d
David Galanter <dgalante@uci.edu>
2014-11-11 00:49:21 -0800

    Controller support! Right now catching and throwing is mapped to the A button. the triggers are analogue and don't as easily translate to the existing code. I'll look into fixing that next.

--- Assets/Scripts/GodController.cs
+++ Assets/Scripts/GodController.cs
@@ -51,7 +51,7 @@ public class GodController : MonoBehaviour {
         }
 
         if (myPlanet != null) { // if your god is holding a planet
-            if (Input.GetButtonDown("Fire" + player)) { //throw planet
+            if (Input.GetButtonDown("Fire" + player) || Input.GetButtonDown("Fire_360_" + player)) { //throw planet
                 releaseButtonFire = false;
                 Rigidbody2D planetBody = myPlanet.GetComponent<Rigidbody2D>();
                 planetBody.simulated = true;  //renable planets physics
@@ -73,7 +73,7 @@ public class GodController : MonoBehaviour {
             }
         }
 
-        if (Input.GetButtonUp("Fire" + player)) {
+		if (Input.GetButtonUp("Fire" + player) || Input.GetButtonUp("Fire_360_" + player)) {
             releaseButtonFire = true;
         }
     }
@@ -83,7 +83,14 @@ public class GodController : MonoBehaviour {
         // define input axis' under Edit->Project Settings->Input
         // so far just 2 player keyboard input set up, but can later expand to 4+ people with controllers
         dx = Input.GetAxis("Horizontal" + player) * god.acceleration;
+		//Making sure players cant press keyboard AND controller for speed boost
+		if (dx == 0.0) {
+			dx = Input.GetAxis ("Horizontal_360_" + player) * god.acceleration;
+				}
         dy = Input.GetAxis("Vertical" + player) * god.acceleration;
+		if (dy == 0.0) {
+			dy = Input.GetAxis("Vertical_360_" + player) * god.acceleration;
+				}
         myRigidbody.AddForce(new Vector2(dx, dy), ForceMode2D.Force);
 
         // limit velocity to maxSpeed of god
@@ -99,7 +106,7 @@ public class GodController : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Planet") {
-            if (Input.GetButton("Fire" + player) && myPlanet == null) {  // catch planet if button is down and we dont have one
+            if ((Input.GetButton("Fire" + player) || Input.GetButton("Fire_360_" + player)) && myPlanet == null) {  // catch planet if button is down and we dont have one
                 if (releaseButtonFire) { // incase you just threw planet and still holding down button you dont want to pick up same one
                     myPlanet = collision.gameObject;
                     Rigidbody2D planetBody = myPlanet.GetComponent<Rigidbody2D>();
