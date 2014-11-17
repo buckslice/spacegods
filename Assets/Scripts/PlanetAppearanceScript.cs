83951b01c4d099f3662859e952055784b8662935
PurplePotassium <mtpawlan@gmail.com>
2014-11-16 22:26:28 -0800

    Random planets and planet shading

---
+++ Assets/Scripts/PlanetAppearanceScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetAppearanceScript : MonoBehaviour {
+
+	public Sprite[] PossibleSprites;
+
+	private Transform model;
+
+	void Start(){
+		//model = transform.Find("Model");
+		transform.GetComponent<SpriteRenderer>().sprite = PossibleSprites[Random.Range(0,PossibleSprites.Length)];
+	}
+
+	void Update () {
+	
+	}
+}
