39007785a94785637d39329abf6b401da189002c
PurplePotassium <mtpawlan@gmail.com>
2014-11-19 18:33:34 -0800

    Added two new planets, removed some planet textures from the pool, added random clouds to planets, improved sun effects, and whateverstuff

--- Assets/Scripts/PlanetClouds.cs
+++ Assets/Scripts/PlanetClouds.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlanetAppearanceScript : MonoBehaviour {
-
+public class PlanetClouds : MonoBehaviour {
+	
 	public Sprite[] PossibleSprites;
-
+	
 	private Transform model;
-
+	
 	void Start(){
 		//model = transform.Find("Model");
 		Sprite sprite = PossibleSprites[Random.Range(0,PossibleSprites.Length)];
-		transform.localScale = new Vector3(2f/sprite.bounds.size.x,2f/sprite.bounds.size.y,1);
+		//transform.localScale = new Vector3(2f/sprite.bounds.size.x,2f/sprite.bounds.size.y,1);
+		transform.Rotate(new Vector3(0,0,Random.value*360));
 		transform.GetComponent<SpriteRenderer>().sprite = sprite;
 	}
-
-	void Update () {
 	
+	void Update () {
+		
 	}
 }
