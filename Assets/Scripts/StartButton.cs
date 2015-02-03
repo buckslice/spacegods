fc1a1e3190445184f0c04e90f26430b33423f10c
Anuj Shah <anujs3@uci.edu>
2015-02-02 22:50:29 -0800

    Added control and exit buttons to the start menu.

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -4,12 +4,16 @@ using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour {
 	public Button button;
+	public string scene_to_load;
 	// Use this for initialization
 	void Start () {
 		button.onClick.AddListener (() => whenClicked ());
 	}
 	void whenClicked(){
-		Application.LoadLevel ("Main");
+		if (scene_to_load.Equals("ExitGame"))
+			Application.Quit ();
+		else
+			Application.LoadLevel (scene_to_load);
 	}
 	// Update is called once per frame
 	void Update () {
